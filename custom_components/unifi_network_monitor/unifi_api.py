46a9a18d4d5f96aee9e9b63800136a7ad5da0b82
James Wilson <jameswilson2@me.com>
2025-08-03 21:25:10 +0100

    Fixing dict merging issue

--- custom_components/unifi_network_monitor/unifi_api.py
+++ custom_components/unifi_network_monitor/unifi_api.py
@@ -62,20 +62,14 @@ class UniFiController:
                                     detail = detail_data.get("data", {})
                                     # Merge detail into device safely
                                     for k, v in detail.items():
-                                        # Only update if v is a dict and not empty, or a primitive
-                                        if k not in device:
+                                        # Only merge if v is a dict (not a list of keys)
+                                        if isinstance(v, dict):
+                                            device[k] = v
+                                        # Only merge primitives (str, int, bool, etc.)
+                                        elif not isinstance(v, (dict, list)):
                                             device[k] = v
                                         else:
-                                            if isinstance(device[k], dict) and isinstance(v, dict) and v:
-                                                device[k].update(v)
-                                            elif isinstance(device[k], dict) and isinstance(v, dict) and not v:
-                                                continue  # Don't overwrite with empty dict
-                                            elif isinstance(device[k], list) and isinstance(v, list):
-                                                continue  # Don't overwrite lists
-                                            elif not isinstance(v, (dict, list)):
-                                                device[k] = v
-                                            else:
-                                               _LOGGER.debug("Skipped merging key %s for device %s (type mismatch or empty)", k, device_id)
+                                            _LOGGER.debug("Skipped merging key %s for device %s (type %s): %s", k, device_id, type(v), v)
                                 else:
                                     _LOGGER.error("Failed to fetch details for device %s: HTTP %d", device_id, detail_resp.status)
                             detailed_devices.append(device)
